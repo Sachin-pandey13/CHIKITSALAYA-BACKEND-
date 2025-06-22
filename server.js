143be65e16bed6288d76e3d44ef727d63d684404
Sachin-pandey13 <aatrox1302@gmail.com>
2025-06-22 16:40:18 +0530

    server.js

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.get('/', (req, res) => {
   res.send('✅ CHIKITSALAYA Backend is running.');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
 module.exports = app;
